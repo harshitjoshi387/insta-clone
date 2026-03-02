0346723ae1d3b6a717b2da60b3166e29ddbfa8fe
Harshit joshi <harshjoshi25022002@gmail.com>
2026-03-02 15:20:48 +0530

    add the middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,24 @@
 const jwt = require('jsonwebtoken')
 
 async function identifyUser(req,res,next){
+    const token = req.cookies.token
+
+     if(!token){
+        return res.status(401).json({
+            message:"UnAuthorized Access"
+        })
+    }
+
+    let decoded = null
+try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+}catch(err){
+        return res.status (401).json({
+            message:"Token invalid"
+        })
+}
+req.user=decoded
+next()
 
 }
 
